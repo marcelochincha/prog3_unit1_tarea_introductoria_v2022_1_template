3c1043dcda48af137222e05f2c3553e1f0217079
RubenRivas <rudrivas@hotmail.com>
2023-01-11 18:20:18 -0500

    Update project

--- autograder/tests/catch_test_1_1/test_1_1.cpp
+++ autograder/tests/catch_test_1_1/test_1_1.cpp
@@ -1,33 +1,32 @@
 //
 // Created by rudri on 9/12/2020.
 //
-#include "catch.hpp"
+#include "catch/catch.hpp"
 #include "redirect_io.h"
 #include "matrix.h"
-#include "number.h"
 #include <random>
 #include <iostream>
 using namespace std;
 
 static void question_1_1() {
-    utec::matrix m1(10, 20);
     random_device rd;
+    utec::matrix m1(10, 20);
+    utec::matrix m2(30, 40);
     uniform_int_distribution<int> dis(0, 100);
-
-    for (int i = 0; i < m1.row_size(); ++i) {
-        for (int j = 0; j < m1.col_size(); ++j) {
-            m1(i, j) = new integer_t(dis(rd));
-        }
+    for (size_t i = 0; i < m1.rows(); ++i) {
+        for (size_t j = 0; j < m1.cols(); ++j)
+            m1(i, j) = dis(rd);
     }
-    utec::matrix m2 = m1;
-
-    for (int i = 0; i < m2.row_size(); ++i) {
-        for (int j = 0; j < m2.col_size(); ++j) {
-            m2(i, j) = new real_t(dis(rd));
-        }
+    utec::matrix m3 = m1;
+    for (size_t i = 0; i < m2.rows(); ++i) {
+        for (size_t j = 0; j < m2.cols(); ++j)
+            m2(i, j) = dis(rd);
     }
-    cout << m1.row_size() << " " << m1.col_size() << endl;
-    cout << m2.row_size() << " " << m2.col_size() << endl;
+    cout << m1.rows() << " " << m1.cols() << endl;
+    cout << m2.rows() << " " << m2.cols() << endl;
+    swap(m1, m2);
+    cout << m1.rows() << " " << m1.cols() << endl;
+    cout << m2.rows() << " " << m2.cols() << endl;
 }
 
 TEST_CASE("Question #1_1") {
--- main.cpp
+++ main.cpp
@@ -1,6 +1,13 @@
 #include <iostream>
+#include <vector>
+#include <random>
+#include "PC1.h"
+using namespace std;
 
 int main() {
-    std::cout << "Hola Mundo" << std::endl;
+    question_1_1();
+    question_1_2();
+    question_1_3();
+    question_1_4();
     return 0;
 }
